6490d761cf94dcf95440d9baf2512cee0f104529
shugohirao <sapphi11@gmail.com>
2018-12-04 18:15:45 +0900

    [add] getRawValue functions

--- src/ofxSocketIOData.h
+++ src/ofxSocketIOData.h
@@ -18,11 +18,21 @@ public :
   void setData(sio::message::ptr const&);
   void setNullData();
 
+  //[*] emit( eventName , value )
+  string getRawString() { return _data->get_string(); }
+  int getRawInt() { return _data->get_int(); }
+  float getRawFloat() { return (float) _data->get_double(); }
+  double getRawDouble() { return _data->get_double(); }
+  bool getRawBool() { return _data->get_bool(); }
+
+  //[*] const mapValue = {valName : value}
+  //    emit( eventName , mapValue )
   std::string getStringValue(std::string key);
   int getIntValue(std::string key);
   float getFloatValue(std::string key);
   double getDoubleValue(std::string key);
   bool getBoolValue(std::string key);
+    
   ofxSocketIOData getNestedValue(std::string key);
 
   std::vector<std::shared_ptr<sio::message>>& getVector();
